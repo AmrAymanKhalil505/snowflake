0688057315b266178b5b8c18856740e36196a194
GehadAbdElhalim <halimgego@gmail.com>
2019-01-26 15:26:44 +0200

    PlayerLogic

---
+++ GameJam/Assets/Gehad/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    Vector3 target;
+    public float followSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = Vector3.Lerp(transform.position, target + new Vector3(-2.5f,5,-4), followSpeed * Time.deltaTime);
+    }
+}
---
+++ GameJam/Assets/Gehad/Scripts/CharacterScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+
+    public float MovementSpeed;
+
+    public float maxHealth;
+
+    public float currentHealth;
+
+    public float HealthDecaySpeed;
+
+    public bool HealthDecay;
+
+    public bool dead;
+
+    public int numOfWood;
+
+    public GameObject Fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fire = GameObject.FindGameObjectWithTag("Fire");
+        numOfWood = 0;
+        dead = false;
+        HealthDecay = false;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            if(currentHealth <= 0)
+            {
+                dead = true;
+            }
+
+            float x = Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime;
+            float z = Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime;
+
+            transform.Translate(x, 0, z);
+
+            if (HealthDecay)
+            {
+                currentHealth -= Time.deltaTime;
+            }
+
+            if (!HealthDecay)
+            {
+                currentHealth = currentHealth;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Home")
+        {
+            if (Fire.GetComponent<FireScript>().intensity > 0)
+            {
+                currentHealth = maxHealth;
+                Fire.GetComponent<FireScript>().intensity += 0.2f * numOfWood;
+                numOfWood = 0;
+                if(Fire.GetComponent<FireScript>().intensity > 1)
+                {
+                    Fire.GetComponent<FireScript>().intensity = 1;
+                }
+                HealthDecay = false;
+            }
+            else
+            {
+                HealthDecay = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Wood")
+        {
+            numOfWood++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Home")
+        {
+            HealthDecay = true;
+        }
+    }
+}
